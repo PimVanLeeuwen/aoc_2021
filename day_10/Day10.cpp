e48967f665c2807d05610d58bb7ea2cf6a078013
Pim van Leeuwen <pimvleeuwen@outlook.com>
2024-11-12 23:55:47 +0100

    Completed day 10

--- day_10/Day10.cpp
+++ day_10/Day10.cpp
@@ -13,13 +13,12 @@
 using namespace std;
 
 void Day10::execute(const vector<string> &lines) {
-
     long part_1 = 0;
     vector<long> part_2_scores;
     long part_2 = 0;
 
     //openings characters
-    vector<char> openings = {'(','[','{','<'};
+    vector<char> openings = {'(', '[', '{', '<'};
     // map scores for part 1
     map<char, int> closing_score = {
         {')', 3},
@@ -36,10 +35,10 @@ void Day10::execute(const vector<string> &lines) {
     };
     // match brackets inner to outer
     map<char, char> closing_match = {
-        {'(',')'},
-        {'[',']'},
-        {'{','}'},
-        {'<','>'},
+        {'(', ')'},
+        {'[', ']'},
+        {'{', '}'},
+        {'<', '>'},
     };
 
     // add the input to the grid
@@ -51,10 +50,10 @@ void Day10::execute(const vector<string> &lines) {
             if (find(openings.begin(), openings.end(), c) != openings.end()) {
                 process.push_back(c);
 
-            // otherwise it should match the opening bracket
+                // otherwise it should match the opening bracket
             } else if (closing_match[process[process.size() - 1]] == c) {
                 process.pop_back();
-            // if this is also not the case, corrupt line
+                // if this is also not the case, corrupt line
             } else {
                 part_1 += closing_score[c];
                 process.clear();
@@ -70,13 +69,12 @@ void Day10::execute(const vector<string> &lines) {
             reverse(process.begin(), process.end());
             for (auto &c: process) {
                 // add the score
-                part_2_local = (part_2_local*5) + closing_score_2[closing_match[c]];
+                part_2_local = (part_2_local * 5) + closing_score_2[closing_match[c]];
             }
 
             // add the score to the list
             part_2_scores.push_back(part_2_local);
         }
-
     }
 
     // get the middle value out of the sorted part 2 array
