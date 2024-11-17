02d530ce3997724c1aed020bf71b04519b507384
Pim van Leeuwen <pimvleeuwen@outlook.com>
2024-11-17 23:29:49 +0100

    Completed day 16 (partially). I have done almost entirety of part 2 but it refuses to give right answer and I am done debugging so here is the attempt and I will move on.

--- day_16/Day16.cpp
+++ day_16/Day16.cpp
@@ -4,17 +4,19 @@
 
 #include "Day16.h"
 
+#include <climits>
 #include <cmath>
 #include <iostream>
 #include <math.h>
+#include <numeric>
 #include <ostream>
 #include <stdexcept>
 
 using namespace std;
 
-int dtoi(const string& decimal) {
+long long dtoi(const string& decimal) {
     size_t count = decimal.length()-1;
-    int result = 0;
+    long long result = 0;
     for (const char c : decimal) {
         result = (c - '0') ? result + static_cast<int>(pow(2, count)) : result;
         count--;
@@ -22,9 +24,118 @@ int dtoi(const string& decimal) {
     return result;
 }
 
+long long process_packet(int& part_1, const string& packet, int& pointer) {
+    // process the head
+    part_1 += dtoi(packet.substr(pointer, 3)); // version
+    const int type_ID = dtoi(packet.substr(pointer+3, 3));
+
+    pointer += 6;
+
+    // literal value
+    if (type_ID == 4) {
+        string number;
+        while (true) {
+            if (packet[pointer] == '1') {
+                number += packet.substr(pointer+1, 4);
+                pointer += 5;
+            } else {
+                number += packet.substr(pointer+1, 4);
+                pointer += 5;
+                break;
+            }
+        }
+        return dtoi(number);
+    }
+
+    // not a literal
+
+    vector<long long> numbers;
+
+    if (packet[pointer] == '1') {
+        pointer++;
+        // number of sub-packets for operator packet
+        const int number_packets = dtoi(packet.substr(pointer, 11));
+        pointer += 11;
+
+        for (int i = 0; i < number_packets; i++) {
+            numbers.push_back(process_packet(part_1, packet, pointer));
+        }
+    } else {
+        pointer++;
+        // total length operator packet
+        const int total_length = dtoi(packet.substr(pointer, 15));
+        pointer += 15;
+        const int start_pointer = pointer;
+
+        while (true) {
+            numbers.push_back(process_packet(part_1, packet, pointer));
+            if (pointer == start_pointer + total_length) {
+                break;
+            }
+        }
+    }
+
+    long long start;
+
+    switch (type_ID) {
+        case 0:
+            // sum; their value is the sum of the values of their sub-packets. If they only have a single sub-packet, their value is the value of the sub-packet
+            start = 0;
+            for (const auto num : numbers) {
+                start += num;
+            }
+            return start;
+        case 1:
+            // product; their value is the result of multiplying together the values of their sub-packets. If they only have a single sub-packet, their value is the value of the sub-packet
+            start = 1;
+            for (const auto num : numbers) {
+                start *= num;
+            }
+
+            return start;
+        case 2:
+            // min; their value is the minimum of the values of their sub-packets
+            start = LLONG_MAX;
+            for (const auto num : numbers) {
+                start = min(start, num);
+            }
+
+            return start;
+        case 3:
+            // max; their value is the maximum of the values of their sub-packets
+            start = LLONG_MIN;
+            for (const auto num : numbers) {
+                start = max(start, num);
+            }
+
+            return start;
+        case 5:
+            // greater than; their value is 1 if the value of the first sub-packet is greater than the value of the second sub-packet; otherwise, their value is 0. These packets always have exactly two sub-packets
+            if (numbers[0] > numbers[1]) {
+                return 1;
+            }
+            return 0;
+        case 6:
+            // less than; their value is 1 if the value of the first sub-packet is less than the value of the second sub-packet; otherwise, their value is 0. These packets always have exactly two sub-packets
+            if (numbers[0] < numbers[1]) {
+                return 1;
+            }
+            return 0;
+        case 7:
+            // equal; their value is 1 if the value of the first sub-packet is equal to the value of the second sub-packet; otherwise, their value is 0. These packets always have exactly two sub-packets
+            if (numbers[0] == numbers[1]) {
+                return 1;
+            }
+            return 0;
+        default:
+            throw domain_error("Invalid input");
+    }
+}
+
 void Day16::execute(const vector<string> &lines) {
-    string input = lines[0];
+    const string& input = lines[0];
     string input_dec;
+    int part_1 = 0;
 
     for (const auto c : input) {
         switch (c) {
@@ -48,39 +159,11 @@ void Day16::execute(const vector<string> &lines) {
         }
     }
 
-    cout << input_dec << endl;
-
     int pointer = 0;
 
-    // process the string
-    while (pointer < input_dec.length()) {
-        // process the head
-        int version = dtoi(input_dec.substr(pointer, 3));
-        int type_ID = dtoi(input_dec.substr(pointer+3, 3));
-        cout << "Version: " << version << endl;
-        cout << "Type_ID: " << type_ID << endl;
+    const long part_2 = process_packet(part_1, input_dec, pointer);
 
-        pointer += 6;
-
-        // literal value
-        if (type_ID == 4) {
-            int literal_count = 6;
-            while (true) {
-                if (input_dec[pointer] == '1') {
-                    cout << input_dec.substr(pointer+1, 4)  << endl;
-                    pointer += 5; literal_count += 5;
-                } else {
-                    cout << input_dec.substr(pointer+1, 4)  << endl;
-                    pointer += 5; literal_count += 5;
-                    break;
-                }
-            }
-
-            // skip the redundant bits
-            pointer = (literal_count % 4 == 0) ? pointer : pointer + 4 - literal_count % 4;
-        } else {
-            // TODO: other case for the operator
-        }
-    }
+    cout << "Part 1: " << part_1 << endl;
+    cout << "Part 2: " << part_2 << endl;
 
 }
